91784465dffece2d1b650691acc7e836fec6c5a4
kimyh5392 <mk.kimyh@gmail.com>
2018-05-09 18:45:43 +0900

    main 및 sub 퍼블리싱 작업중 . . .

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
 <link rel="stylesheet" href="/assets/css/main.css">
 </head>
 <body>
@@ -7,13 +8,367 @@
     
     <!-- Content -->
     <main id="content">
-        <div class="container-fluid">
-            <h1>Content Start</h1>
+        <!-- jumbotrons slider -->
+        <div class="jumbotrons">
+            <div class="visual-list slide-1">
+                <div class="container-fluid">
+                    <div class="details">
+                        <small>MISSION</small>
+                        <h3>나누는 사랑 넘치는 기쁨</h3>
+                        <p>
+                            굿피플은 가난, 재난, 질병으로부터 구제하고 <br class="d-md-none">
+                            자립할 수 있도록 지원하여<br class="d-none d-md-block"> 모두가 고통받지 않는 <br class="d-md-none">
+                            행복한 세상을 만들기 위해 존재합니다.
+                        </p>
+                        <div class="btn-area">
+                            <p>
+                                <a href="#" class="btn btn-blue" role="button">자세히보기</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="visual-list slide-2">
+                <div class="container-fluid">
+                    <div class="details">
+                        <small>MISSION</small>
+                        <h3>나누는 사랑 넘치는 기쁨</h3>
+                        <p>
+                            굿피플은 가난, 재난, 질병으로부터 구제하고 <br class="d-md-none">
+                            자립할 수 있도록 지원하여<br class="d-none d-md-block"> 모두가 고통받지 않는 <br class="d-md-none">
+                            행복한 세상을 만들기 위해 존재합니다.
+                        </p>
+                        <div class="btn-area">
+                            <p>
+                                <a href="#" class="btn btn-blue" role="button">자세히보기</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="visual-list slide-3">
+                <div class="container-fluid">
+                    <div class="details">
+                        <small>MISSION</small>
+                        <h3>나누는 사랑 넘치는 기쁨</h3>
+                        <p>
+                            굿피플은 가난, 재난, 질병으로부터 구제하고 <br class="d-md-none">
+                            자립할 수 있도록 지원하여<br class="d-none d-md-block"> 모두가 고통받지 않는 <br class="d-md-none">
+                            행복한 세상을 만들기 위해 존재합니다.
+                        </p>
+                        <div class="btn-area">
+                            <p>
+                                <a href="#" class="btn btn-blue" role="button">자세히보기</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+        
+        <!-- support-business -->
+        <article class="support-business">
+            <div class="business domestic">
+                <p class="participation or">524 <small class="or">명</small></p>
+                <h3>국내아동후원사업</h3>
+                <blockquote class="d-none d-md-block">
+                    국내 요보호아동과 시설아동들의 아동후원
+                    및 지원사업을 전국의 그룹홈 및 지역아동
+                    센터, 공부방과 연계하여 지원하고 있습니다.
+                </blockquote>
+            </div>
+            <div class="business overseas">
+                <p class="participation bl">6,384 <small class="bl">명</small></p>
+                <h3>해외아동후원사업</h3>
+                <blockquote class="d-none d-md-block">
+                    아동들의 생활개선, 기초교육, 의료지원, 영양공급
+                    등을 통해 신체적, 정서적, 사회적으로 건강하게
+                    성장할 수 있도록 지원하며, 더 나은 삶을 실현할
+                    수 있도록 도와줍니다
+                </blockquote>
+            </div>
+            <div class="business medical">
+                <p class="participation or">5,830 <small class="or">명</small></p>
+                <h3>사랑의 의료봉사</h3>
+                <blockquote class="d-none d-md-block">
+                    이동진료차량으로 의료사각지대에 거주하는
+                    가난하고 소외된 이웃에게 직접 찾아가 방문진료, 
+                    건강검진 등 전문화된 의료혜택을 제공하고
+                    있습니다.
+                </blockquote>
+            </div>
+            <div class="business sanitation">
+                <p class="participation bl">2,037 <small class="bl">명</small></p>
+                <h3>해외 보건의료사업</h3>
+                <blockquote class="d-none d-md-block">
+                    보건 및 위생환경이 열악한 지역을 대상으로
+                    의료서비스를 제공하며 현지에 병원개설,
+                    이동진료 등을 통해 의료환경을 개선합니다.
+                </blockquote>
+            </div>
+        </article>
+        
+        <!-- campaign -->
+        <section class="campaign-list">
+            <div class="container-fluid">
+                <h3>굿피플 캠페인</h3>
+                <div class="section-content">
+                    <ul class="row">
+                        <li class="col-12 col-md-6 col-xl-4">
+                            <a href="#">
+                                <div class="item">
+                                    <p class="ongoing">진행중</p>
+                                    <img src="/assets/images/main/img_campaign01.jpg" class="img-fluid" alt="">
+                                </div>
+                                <div class="details">
+                                    <div class="progress-area">
+                                        <div class="state-bar" style="width: 70%;">
+                                            <p class="d-day">D-7</p>
+                                        </div>
+                                    </div>
+                                    <h4>
+                                        <small>사랑의 의료봉사</small>
+                                        구멍난 뼈, 위험신호를 알려요
+                                    </h4>
+                                    <p class="more">자세히 보기</p>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="col-12 col-md-6 col-xl-4">
+                            <a href="#">
+                                <div class="item">
+                                    <p class="ongoing">진행중</p>
+                                    <img src="/assets/images/main/img_campaign02.jpg" class="img-fluid" alt="">
+                                </div>
+                                <div class="details">
+                                    <div class="progress-area">
+                                        <div class="state-bar" style="width: 70%;">
+                                            <p class="d-day">D-7</p>
+                                        </div>
+                                    </div>
+                                    <h4>
+                                        <small>국내아동지원</small>
+                                        위풍당당 새학기
+                                    </h4>
+                                    <p class="more">자세히 보기</p>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="d-none d-md-block col-md-6 col-xl-4">
+                            <a href="#">
+                                <div class="item">
+                                    <p class="ongoing">진행중</p>
+                                    <img src="/assets/images/main/img_campaign03.jpg" class="img-fluid" alt="">
+                                </div>
+                                <div class="details">
+                                    <div class="progress-area">
+                                        <div class="state-bar" style="width: 70%;">
+                                            <p class="d-day">D-7</p>
+                                        </div>
+                                    </div>
+                                    <h4>
+                                        <small>해외아동결연</small>
+                                        여전히 굶주린 세계
+                                    </h4>
+                                    <p class="more">자세히 보기</p>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="d-none d-md-block d-xl-none col-12 col-md-6 col-xl-4">
+                            <a href="#">
+                                <div class="item">
+                                    <p class="ongoing">진행중</p>
+                                    <img src="/assets/images/main/img_campaign01.jpg" class="img-fluid" alt="">
+                                </div>
+                                <div class="details">
+                                    <div class="progress-area">
+                                        <div class="state-bar" style="width: 70%;">
+                                            <p class="d-day">D-7</p>
+                                        </div>
+                                    </div>
+                                    <h4>
+                                        <small>사랑의 의료봉사</small>
+                                        구멍난 뼈, 위험신호를 알려요
+                                    </h4>
+                                    <p class="more">자세히 보기</p>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                    <div class="btn-area d-xl-none">
+                        <p>
+                            <a href="#" class="btn btn-gray" role="button">더 많은 캠페인 보기</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        <!-- goodpeople-news -->
+        <article class="goodpeople-news">
+            <div class="news">
+                <a href="#">
+                    <div class="item-img">
+                        <img src="/assets/images/main/img_news01.jpg" class="img-fluid" alt="">
+                    </div>
+                    <div class="details">
+                        <p class="news-label">굿피플 소식</p>
+                        <h3>2018년 기부 마라톤 대회</h3>
+                    </div>
+                </a>
+            </div>
+            <div class="news">
+                <a href="#">
+                    <div class="item-img">
+                        <img src="/assets/images/main/img_news02.jpg" class="img-fluid" alt="">
+                    </div>
+                    <div class="details">
+                        <p class="news-label">굿피플 소식</p>
+                        <h3>해외아동결연 아동 어린이날 선물금 안내</h3>
+                    </div>
+                </a>
+            </div>
+            <div class="news">
+                <a href="#">
+                    <div class="item-img">
+                        <img src="/assets/images/main/img_news03.jpg" class="img-fluid" alt="">
+                    </div>
+                    <div class="details">
+                        <p class="news-label">굿피플 소식</p>
+                        <h3>2018 굿피플 힐링콘서트 개최</h3>
+                    </div>
+                </a>
+            </div>
+        </article>
+        
+        <!-- campaign-review -->
+        <article class="campaign-review">
+            <div class="container-fluid">
+                <div class="review-slide">
+                    
+                </div>
+            </div>
+        </article>
+        
     </main>
     
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/sitemap.php'); ?>
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <script>
+        $(function(){
+            // jumbotrons visual Slider
+            $(window).ready(function (){
+                $('.jumbotrons').slick({
+                    dots: true,
+                    infinite: true,
+                    autoplaySpeed: 5000,
+                    cssEase: 'linear',
+                    pauseOnHover: false,
+                    pauseOnFocus: false,
+                });
+            });
+            $('.jumbotrons').on('afterChange', function(event, slick, currentSlide, nextSlide){
+                $(".slick-slide").removeClass("action");
+                setTimeout(function(){ 
+                    $(".slick-active").addClass("action"); 
+                    /*console.log('init'); */
+                }, 500);
+            });
+            $(".jumbotrons").on("init", function(event,slick){
+                setTimeout(function(){ 
+                    $(".slick-active").addClass("action"); 
+                    /*console.log('init'); */
+                }, 500);
+            });
+            
+            // support-business
+            $('.support-business').slick({
+                autoplay: false,
+                dots: false,
+                infinite: false,
+                speed: 300,
+                slidesToShow: 4,
+                slidesToScroll: 4,
+                responsive: [
+                {
+                    breakpoint: 1200,
+                    settings: {
+                        autoplay: false,
+                        slidesToShow: 2.5,
+                        slidesToScroll: 1,
+                        dots: false,
+                        infinite: false,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                        autoplay: false,
+                        dots: false,
+                        infinite: false,
+                    }
+                },
+                {
+                    breakpoint: 320,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                        autoplay: false,
+                        dots: false,
+                        infinite: false,
+                    }
+                }
+                ]
+            });
+            
+            // support-business
+            $('.goodpeople-news').slick({
+                autoplay: false,
+                dots: false,
+                infinite: false,
+                speed: 300,
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                responsive: [
+                {
+                    breakpoint: 1200,
+                    settings: {
+                        autoplay: false,
+                        slidesToShow: 1.5,
+                        slidesToScroll: 1,
+                        dots: false,
+                        infinite: false,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        autoplay: false,
+                        dots: false,
+                        infinite: false,
+                    }
+                },
+                {
+                    breakpoint: 320,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        autoplay: false,
+                        dots: false,
+                        infinite: false,
+                    }
+                }
+                ]
+            });
+            
+            
+        });
+    </script>
 </body>
 </html>
