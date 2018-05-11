542271502d1d5370a337890fa3b1614f87f4c549
kimyh5392 <mk.kimyh@gmail.com>
2018-05-11 17:24:45 +0900

    greetings 완료

--- introduce/greetings.php
+++ introduce/greetings.php
@@ -34,9 +34,48 @@
     
     <!-- Content -->
     <main id="content" class="greetings">
-    	<div class="container">
- 			
-    	</div>
+        <section class="ceo-message">
+            <div class="container">
+                <h3>
+                    안녕하세요.<br>
+                    굿피플 이사장 <strong>이영훈</strong>입니다.
+                </h3>
+                <div class="row">
+                    <div class="col-12 col-xl-8">
+                        <p>
+                            날마다 변화하는 놀라운 세상을 살아가고 있습니다. 더욱이 지구촌 곳곳에서 일어나는 전쟁과 자연 재해로 인해 우리의 이웃은 가난과 질병, 기아 등생존에 극심한 위협으로 고통 받고 있습니다.<br><br>
+                        </p>
+                    </div>
+                    <div class="col-12 col-md-7 col-xl-8">
+                        <p>
+                            지구촌 곳곳을 찾아다니면서 아직도 하루 한 끼로 생활 하는 아이들, 물 한 방울을 얻기 위해 몇 시간씩 걸어가야 하는 사람들,  간단한 의약품조차 없어 내일을 보장 할 수 없는 사람들을 만났습니다. 그들에게 우리의 작은 사랑이 희망이 되고 있습니다.<br><br>
+                            앞으로도 세상의 고통 속에 살아가는 사람들 모두가 웃을 때까지 사랑하겠습니다.  굿피플이 사람의 가치를 높이며, 희망을 전할 수 있도록 지속적인 응원 부탁드립니다.
+                        </p>
+                        <dl>
+                            <dt>사단법인 굿피플인터내셔널 이사장</dt>
+                            <dd><img src="/assets/images/introduce/img_signature.png" alt="이영훈"></dd>
+                        </dl>
+                    </div>
+                    <div class="col-12 col-md-5 col-xl-4">
+                        <figure>
+                            <img src="/assets/images/introduce/img_ceo.png" class="img-fluid" alt="굿피플 이사장 이영훈">
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <article class="vision">
+            <div class="container">
+                <figure>
+                    <img src="/assets/images/introduce/img_logo.png" alt="굿피플 로고">
+                </figure>
+                <p>
+                    세상 모든 사람들이 웃을 때까지 사랑하며<br>
+                    지구촌 어려운 이웃사회에 희망을 세워 나갑니다.
+                </p>
+                <small>세상을 변화시키는 행복한 에너지</small>
+            </div>
+        </article>
     </main>
     
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/sitemap.php'); ?>
