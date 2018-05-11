014800a3fed5281b45b8bd39e93bdd4e85b0b641
kimyh5392 <mk.kimyh@gmail.com>
2018-05-11 16:12:32 +0900

    introduce modify

--- introduce/greetings.php
+++ introduce/greetings.php
@@ -12,47 +12,7 @@
             <h2>We bring <span>hope</span> to neighbors<br>in despair around the world</h2>
         </div>
         <div class="container">
-            <div class="page-path">
-                <ol class="breadcrumb">
-                    <li class="d-none d-md-block home">
-                        <a href="/">
-                            <i class="icon-home"><span class="sr-only">메인이동</span></i>
-                        </a>
-                    </li>
-                    <li>
-                        <div class="dropdown depth-1">
-                            <button class="btn btn-secondary dropdown-toggle" type="button" id="depth-list-1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                굿피플소개
-                            </button>
-                            <div class="dropdown-menu" aria-labelledby="depth-list-1">
-                                <a class="dropdown-item" href="#">사업소개</a>
-                                <a class="dropdown-item" href="#">캠페인</a>
-                                <a class="dropdown-item" href="#">소식</a>
-                                <a class="dropdown-item" href="#">후원</a>
-                                <a class="dropdown-item" href="#">나의 후원</a>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="dropdown depth-2">
-                            <button class="btn btn-secondary dropdown-toggle" type="button" id="depth-list-1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                소개
-                            </button>
-                            <div class="dropdown-menu" aria-labelledby="depth-list-1">
-                                <a class="dropdown-item" href="#">소개</a>
-                                <a class="dropdown-item" href="#">Good 피플</a>
-                                <a class="dropdown-item" href="#">투명경영</a>
-                                <a class="dropdown-item" href="#">지부안내</a>
-                                <a class="dropdown-item" href="#">찾아오시는 길</a>
-                            </div>
-                        </div>
-                    </li>
-                </ol>
-                <div class="d-none d-xl-block zoom">
-                   <a href="javascript:;" class="in" role="button">확대</a>
-                   <a href="javascript:;" class="out" role="button">축소</a>
-                </div>
-            </div>
+            <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/_lnb.php'); ?>
         </div>
     </div>
     
