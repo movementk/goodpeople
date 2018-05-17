91d0145bfd1750001690c3cd974ad367be192562
kimyh5392 <mk.kimyh@gmail.com>
2018-05-17 17:16:27 +0900

    footer 폴더 완료

---
+++ policy/news_list.php
@@ -0,0 +1,228 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link rel="stylesheet" href="/assets/css/sub.css">
+<link rel="stylesheet" href="/assets/css/policy.css">
+</head>
+<body class="sub policy">
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/aside.php'); ?>
+    
+    <!-- page-header -->
+    <div class="page-header">
+        <div class="page-summary">
+            <h2>We bring <span>hope</span> to neighbors<br>in despair around the world</h2>
+        </div>
+        <div class="container">
+            <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/_lnb.php'); ?>
+        </div>
+    </div>
+    
+    <!-- page-intro -->
+    <div class="page-intro">
+        <div class="container">
+            <div class="page-title">
+                <h3>소식지</h3>
+                <p class="summary">콩닥 콩닥 가슴 뛰는 굿피플의 지구촌 감동 스토리와 함께하세요.</p>
+            </div>
+        </div>
+    </div>
+    
+    <!-- Content -->
+    <main id="content" class="news-list">
+    	<div class="container">
+            <!-- 모바일 태블릿 -->
+            <article class="news-table-sm d-xl-none">
+                <div class="ebook-list">
+                    <div class="news-list">
+                        <a href="#">
+                            <div class="details">
+                                <small>2018년 4~6월호</small>
+                                <h3>2018 Spring Vol.12</h3>
+                            </div>
+                            <div class="item-ebook">
+                                <img src="/assets/images/sub/img_book.jpg" class="img-fluid" alt="">
+                                <div class="hover-box"></div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="news-list">
+                        <a href="#">
+                            <div class="details">
+                                <small>2018년 4~6월호</small>
+                                <h3>2018 Spring Vol.12</h3>
+                            </div>
+                            <div class="item-ebook">
+                                <img src="/assets/images/sub/img_book.jpg" class="img-fluid" alt="">
+                                <div class="hover-box"></div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="backdrop"></div>
+                </div>
+                <div class="ebook-list">
+                    <div class="news-list">
+                        <a href="#">
+                            <div class="details">
+                                <small>2018년 4~6월호</small>
+                                <h3>2018 Spring Vol.12</h3>
+                            </div>
+                            <div class="item-ebook">
+                                <img src="/assets/images/sub/img_book.jpg" class="img-fluid" alt="">
+                                <div class="hover-box"></div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="news-list">
+                        <a href="#">
+                            <div class="details">
+                                <small>2018년 4~6월호</small>
+                                <h3>2018 Spring Vol.12</h3>
+                            </div>
+                            <div class="item-ebook">
+                                <img src="/assets/images/sub/img_book.jpg" class="img-fluid" alt="">
+                                <div class="hover-box"></div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="backdrop"></div>
+                </div>
+            </article>
+            
+            <!-- 웹 -->
+            <article class="news-table-xl d-none d-xl-block">
+                <div class="ebook-list">
+                    <div class="news-list">
+                        <a href="#">
+                            <div class="details">
+                                <small>2018년 4~6월호</small>
+                                <h3>2018 Spring Vol.12</h3>
+                            </div>
+                            <div class="item-ebook">
+                                <img src="/assets/images/sub/img_book.jpg" class="img-fluid" alt="">
+                                <div class="hover-box"></div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="news-list">
+                        <a href="#">
+                            <div class="details">
+                                <small>2018년 4~6월호</small>
+                                <h3>2018 Spring Vol.12</h3>
+                            </div>
+                            <div class="item-ebook">
+                                <img src="/assets/images/sub/img_book.jpg" class="img-fluid" alt="">
+                                <div class="hover-box"></div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="news-list">
+                        <a href="#">
+                            <div class="details">
+                                <small>2018년 4~6월호</small>
+                                <h3>2018 Spring Vol.12</h3>
+                            </div>
+                            <div class="item-ebook">
+                                <img src="/assets/images/sub/img_book.jpg" class="img-fluid" alt="">
+                                <div class="hover-box"></div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="backdrop"></div>
+                </div>
+                <div class="ebook-list">
+                    <div class="news-list">
+                        <a href="#">
+                            <div class="details">
+                                <small>2018년 4~6월호</small>
+                                <h3>2018 Spring Vol.12</h3>
+                            </div>
+                            <div class="item-ebook">
+                                <img src="/assets/images/sub/img_book.jpg" class="img-fluid" alt="">
+                                <div class="hover-box"></div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="news-list">
+                        <a href="#">
+                            <div class="details">
+                                <small>2018년 4~6월호</small>
+                                <h3>2018 Spring Vol.12</h3>
+                            </div>
+                            <div class="item-ebook">
+                                <img src="/assets/images/sub/img_book.jpg" class="img-fluid" alt="">
+                                <div class="hover-box"></div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="news-list">
+                        <a href="#">
+                            <div class="details">
+                                <small>2018년 4~6월호</small>
+                                <h3>2018 Spring Vol.12</h3>
+                            </div>
+                            <div class="item-ebook">
+                                <img src="/assets/images/sub/img_book.jpg" class="img-fluid" alt="">
+                                <div class="hover-box"></div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="backdrop"></div>
+                </div>
+            </article>
+            
+            <!-- paging -->
+            <nav class="paging">
+   				<ol class="pagination">
+   					<li class="first">
+   						<a href="#">
+   							<i class="icon-angle-double-left">
+   								<span class="sr-only">첫 페이지</span>
+   							</i>
+   						</a>
+   					</li>
+   					<li class="prev">
+   						<a href="#">
+   							<i class="icon-angle-left">
+   								<span class="sr-only">이전 페이지</span>
+   							</i>
+   						</a>
+   					</li>
+   					<li class="active">
+   						<a href="#">1</a>
+   					</li>
+   					<li>
+   						<a href="#">2</a>
+   					</li>
+   					<li>
+   						<a href="#">3</a>
+   					</li>
+   					<li>
+   						<a href="#">4</a>
+   					</li>
+   					<li>
+   						<a href="#">5</a>
+   					</li>
+   					<li class="next">
+   						<a href="#">
+   							<i class="icon-angle-right">
+   								<span class="sr-only">다음 페이지</span>
+   							</i>
+   						</a>
+   					</li>
+   					<li class="last">
+   						<a href="#">
+   							<i class="icon-angle-double-right">
+   								<span class="sr-only">마지막 페이지</span>
+   							</i>
+   						</a>
+   					</li>
+   				</ol>	
+   			</nav>
+        </div>
+    </main>
+    
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/sitemap.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+    <script src="/assets/js/sub.js"></script>
+</body>
+</html>
---
+++ policy/terms.php
@@ -0,0 +1,70 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link rel="stylesheet" href="/assets/css/sub.css">
+<link rel="stylesheet" href="/assets/css/policy.css">
+</head>
+<body class="sub policy">
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/aside.php'); ?>
+    
+    <!-- page-header -->
+    <div class="page-header">
+        <div class="page-summary">
+            <h2>We bring <span>hope</span> to neighbors<br>in despair around the world</h2>
+        </div>
+        <div class="container">
+            <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/_lnb.php'); ?>
+        </div>
+    </div>
+    
+    <!-- page-intro -->
+    <div class="page-intro">
+        <div class="container">
+            <div class="page-title">
+                <h3>이용약관</h3>
+            </div>
+        </div>
+    </div>
+    
+    <!-- Content -->
+    <main id="content" class="clause-common">
+    	<div class="container">
+    	    <article class="terms">
+    	        <h2>굿피플 이용약관</h2>
+    	        제 1 조 (목적)<br>
+                본 이용약관(이하 '약관'이라 함)은 사단법인 굿피플인터내셔널(이하 '우리 기관'이라 함)과 이용 고객(이하 '회원'이라 함)간에 우리 기관이 제공하는 인터넷 관련 서비스(http://www.goodpeople.or.kr을 말하며 이하 '서비스'라 함)를 이용함에 있어 회원과 우리 기관간의 권리, 의무 및 책임사항 등 기본적인 제반 사항과 기타 필요한 사항을 구체적으로 규정함을 목적으로 합니다.<br><br>
+
+                제 2 조 (용어의 정의)<br>
+                (1) 본 약관에서 사용하는 용어의 정의는 다음과 같습니다. 
+                - '회원'이라 함은 본 약관에 동의하고 서비스를 이용하는 이용자를 말합니다.
+                - '정기후원회원'이라 함은 매월 정기적으로 기부금을 납부하는 회원을 말합니다.
+                - '이용계약'이라 함은 본 약관을 포함하여 서비스 이용과 관련하여 우리 기관과 회원 간에 체결하는 모든 계약을 말합니다.<br><br>
+                
+                제 3 조 (이용약관의 효력 및 변경) <br>
+                (1) 본 약관은 서비스를 통해 온라인으로 공시하고 회원의 동의와 우리 기관의 승낙으로 효력을 발생합니다.
+                (2) 우리 기관은 합리적인 사유가 발생할 경우 관련 법령에 위배되지 않는 범위 안에서 이용약관을 개정할 수 있습니다. 개정된 약관은 정당한 절차에 따라 서비스를 통해 공지함으로써 효력을 발휘합니다.
+                (3) 회원은 정기적으로 서비스를 방문하여 약관의 변경사항을 확인하여야 합니다. 변경된 약관에 대한 정보를 알지 못해 발생하는 회원의 피해는 우리 기관에서 책임지지 않습니다.<br><br>
+                제 4 조 (약관 외 준칙)<br>
+                (1) 본 약관에 명시되지 않은 사항에 대해서는 전기통신기본법 등 그 외 관계법령에 의합니다.
+                (2) 우리 기관은 필요한 경우 서비스 내의 개별항목에 대하여 개별약관 또는 운영원칙(이하 '서비스별 안내'라 합니다)를 정할 수 있으며, 본 약관과 서비스별 안내의 내용이 상충되는 경우에는 서비스별 안내의 내용을 우선하여 적용합니다.<br><br>
+                제 4 조 (약관 외 준칙)<br>
+                (1) 본 약관에 명시되지 않은 사항에 대해서는 전기통신기본법 등 그 외 관계법령에 의합니다.
+                (2) 우리 기관은 필요한 경우 서비스 내의 개별항목에 대하여 개별약관 또는 운영원칙(이하 '서비스별 안내'라 합니다)를 정할 수 있으며, 본 약관과 서비스별 안내의 내용이 상충되는 경우에는 서비스별 안내의 내용을 우선하여 적용합니다.<br><br>
+                제 4 조 (약관 외 준칙)<br>
+                (1) 본 약관에 명시되지 않은 사항에 대해서는 전기통신기본법 등 그 외 관계법령에 의합니다.
+                (2) 우리 기관은 필요한 경우 서비스 내의 개별항목에 대하여 개별약관 또는 운영원칙(이하 '서비스별 안내'라 합니다)를 정할 수 있으며, 본 약관과 서비스별 안내의 내용이 상충되는 경우에는 서비스별 안내의 내용을 우선하여 적용합니다.<br><br>
+                제 4 조 (약관 외 준칙)<br>
+                (1) 본 약관에 명시되지 않은 사항에 대해서는 전기통신기본법 등 그 외 관계법령에 의합니다.
+                (2) 우리 기관은 필요한 경우 서비스 내의 개별항목에 대하여 개별약관 또는 운영원칙(이하 '서비스별 안내'라 합니다)를 정할 수 있으며, 본 약관과 서비스별 안내의 내용이 상충되는 경우에는 서비스별 안내의 내용을 우선하여 적용합니다.<br><br>
+                제 4 조 (약관 외 준칙)<br>
+                (1) 본 약관에 명시되지 않은 사항에 대해서는 전기통신기본법 등 그 외 관계법령에 의합니다.
+                (2) 우리 기관은 필요한 경우 서비스 내의 개별항목에 대하여 개별약관 또는 운영원칙(이하 '서비스별 안내'라 합니다)를 정할 수 있으며, 본 약관과 서비스별 안내의 내용이 상충되는 경우에는 서비스별 안내의 내용을 우선하여 적용합니다.
+    	    </article>
+    	</div>
+    </main>
+    
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/sitemap.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+    <script src="/assets/js/sub.js"></script>
+</body>
+</html>
