97d40797c0666fcf39bada73c8a7d89d468ca8a2
Joahra <mk_ahra0522@gmail.com>
2018-05-18 14:15:26 +0900

    게시판 뷰 수정 완료.

--- sub.php
+++ sub.php
@@ -406,6 +406,86 @@
            		</div>
            	</article>
            	
+           	<br><br><br>
+           	
+           	<article class="board-view">
+           		<div class="article-header">
+           			<h4>사랑의 의료봉사, 고려대학교 교우회 감사패 수상</h4>
+           			<ul class="icon-list">
+           				<li class="user">굿피플</li>
+           				<li class="calendar">2018-05-01</li>
+						<li class="eye">1,234</li>
+           			</ul>
+           		</div>
+           		<div class="article-content">
+					<!--
+						.view-content  -> width: 100%;
+						.space 추가 했을때 -> width: 725px; center정렬
+					 -->
+           			<div class="view-content space">
+           				<div class="item">
+           					<img src="/assets/images/community/img_news_view01.jpg" class="img-fluid" alt="">
+           					<p>
+								1999년부터 국내 의료 혜택이 미치지 못하는 
+								산간벽지의 독거노인, 조손 가정 및 외국인 근로자, 
+								장애인, 노숙인들에게 매주 무료 진료를 진행해온 굿피플 
+								사랑의의료봉사가 감사패를 받았습니다!
+           					</p>
+           				</div>
+           				<div class="item">
+           					<img src="/assets/images/community/img_news_view02.jpg" class="img-fluid" alt="">
+           					<p>
+								굿피플의 대표적인 보건의료사업 사랑의의료봉사는 
+								지난 10년간 고대교우 의료봉사회와 함께 지역 
+								사회복지관, 북한에서 이탈해 대한민국에 정착한 새터민, 
+								어려움을 겪고 있는 다문화가족 등 의료복지 사각지대의 
+								의료진료를 위해 힘써왔습니다. 
+								이에 33만 고려대 교우들이 사랑의의료봉사의 
+								공로를 인정하고 감사패를 수여했습니다.
+           					</p>
+           				</div>
+           			</div>
+           			<ul class="attach">
+						<li>
+							<a href="#">첨부파일01_파일명.hwp</a>
+						</li>
+						<li>
+							<a href="#">첨부파일01_파일명.hwp</a>
+						</li>
+						<li>
+							<a href="#">첨부파일01_파일명.hwp</a>
+						</li>
+					</ul>
+           			<div class="btn-area">
+           				<p>
+           					<a href="#" class="btn btn-gray" role="button">삭제하기</a>
+           					<a href="#" class="btn btn-gray" role="button">수정하기</a>
+           					<a href="#" class="btn btn-black" role="button">목록으로</a>
+           				</p>
+					</div>
+           			<div class="test-link-list">
+           				<ul>
+           					<li class="t-prev">
+           						<dl class="row">
+           							<dt class="d-none d-md-block col-2">다음글</dt>
+           							<dd class="col-12 col-md-10">
+           								<a href="#">다음글이 없습니다.</a>
+           							</dd>
+           						</dl>
+           					</li>
+           					<li class="t-next">
+           						<dl class="row">
+           							<dt class="d-none d-md-block col-2">이전글</dt>
+           							<dd class="col-12 col-md-10">
+           								<a href="#">굿피플과 함께하는 KCBL 연예인 농구대회  굿피플과 함께하는 KCBL 연예인 농구대회</a>
+           							</dd>
+           						</dl>
+           					</li>
+           				</ul>
+           			</div>
+           		</div>
+           	</article>
+           	
            	<p class="ex-txt">게시판 작성</p>
          	<!--  board 게시판 작성-->
          	<div class="write-wrap">
