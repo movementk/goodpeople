81d5120b99017d07e068a4a1ac9d5765df9e86a5
kimyh5392 <mk.kimyh@gmail.com>
2018-05-21 17:08:02 +0900

    ie9 ie8 stylesheet modify

--- inc/dochead.php
+++ inc/dochead.php
@@ -3,8 +3,15 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>굿피플</title>
     <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="/assets/scrollbar-plugin/jquery.mCustomScrollbar.min.css">
     <link rel="stylesheet" href="/assets/fontello/css/fontello.css">
     <link rel="stylesheet" href="/assets/css/default.css">
-    <title>굿피플</title>
+    <!--[if IE 9]>
+        <link href="https://cdn.jsdelivr.net/gh/coliff/bootstrap-ie8/css/bootstrap-ie9.min.css" rel="stylesheet">
+    <![endif]-->
+    <!--[if lte IE 8]>
+        <link href="https://cdn.jsdelivr.net/gh/coliff/bootstrap-ie8/css/bootstrap-ie8.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/g/html5shiv@3.7.3"></script>
+    <![endif]-->
