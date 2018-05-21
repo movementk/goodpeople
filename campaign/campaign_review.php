5cef0a1bdedde5b6922ff828cccf7dd5c8d4d49c
kimyh5392 <mk.kimyh@gmail.com>
2018-05-21 18:07:37 +0900

    캠페인 완료

---
+++ campaign/campaign_review.php
@@ -0,0 +1,209 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link rel="stylesheet" href="/assets/css/sub.css">
+<link rel="stylesheet" href="/assets/css/campaign.css">
+</head>
+<body class="sub campaign">
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/aside.php'); ?>
+    
+    <!-- page-header -->
+    <div class="page-header">
+        <div class="page-summary">
+            <h2>We bring <span>hope</span> to neighbors<br>in despair around the world</h2>
+        </div>
+        <div class="container">
+            <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/_lnb.php'); ?>
+        </div>
+    </div>
+    
+    <!-- page-intro -->
+    <div class="page-intro">
+        <div class="container">
+            <div class="page-title">
+                <h3>캠페인 후기</h3>
+            </div>
+        </div>
+    </div>
+    
+    <!-- Content -->
+    <main id="content">
+    	<div class="container">
+            <article class="campaign-review">
+                <div class="article-header">
+                    <div class="search-form">
+                        <form action="#">
+                            <div class="form-group">
+                                <input type="text" id="search" class="form-control" placeholder="검색어를 입력해 주세요">
+                                <label for="search" class="sr-only">검색하기</label>
+                            </div>
+                            <p class="btn-search">
+                                <button type="submit" class="btn btn-default">
+                                    <i class="icon-search"></i>
+                                </button>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+                <div class="article-content">
+                    <ul class="row">
+                        <li class="col-12 col-md-6 col-xl-4">
+                            <a href="#">
+                                <div class="item">
+                                    <figure>
+                                        <img src="/assets/images/campaign/img_review01.jpg" class="img-fluid" alt="">
+                                    </figure>
+                                </div>
+                                <div class="details">
+                                    <small>해외아동결연</small>
+                                    <h3>사랑의의료봉사, 고려대학교 교우회 감사패 수상사랑의의료봉사, 고려대학교 교우회 감사패 수상</h3>
+                                    <ul class="icon-list">
+                                        <li class="calendar">2018-05-01</li>
+                                        <li class="eye">1,234</li>
+                                    </ul>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="col-12 col-md-6 col-xl-4">
+                            <a href="#">
+                                <div class="item">
+                                    <figure>
+                                        <img src="/assets/images/campaign/img_review02.jpg" class="img-fluid" alt="">
+                                    </figure>
+                                </div>
+                                <div class="details">
+                                    <small>해외아동결연</small>
+                                    <h3>사랑의의료봉사, 고려대학교 교우회 감사패 수상</h3>
+                                    <ul class="icon-list">
+                                        <li class="calendar">2018-05-01</li>
+                                        <li class="eye">1,234</li>
+                                    </ul>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="col-12 col-md-6 col-xl-4">
+                            <a href="#">
+                                <div class="item">
+                                    <figure>
+                                        <img src="/assets/images/campaign/img_review03.jpg" class="img-fluid" alt="">
+                                    </figure>
+                                </div>
+                                <div class="details">
+                                    <small>해외아동결연</small>
+                                    <h3>사랑의의료봉사, 고려대학교 교우회 감사패 수상</h3>
+                                    <ul class="icon-list">
+                                        <li class="calendar">2018-05-01</li>
+                                        <li class="eye">1,234</li>
+                                    </ul>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="d-none d-md-block col-md-6 col-xl-4">
+                            <a href="#">
+                                <div class="item">
+                                    <figure>
+                                        <img src="/assets/images/campaign/img_review01.jpg" class="img-fluid" alt="">
+                                    </figure>
+                                </div>
+                                <div class="details">
+                                    <small>해외아동결연</small>
+                                    <h3>사랑의의료봉사, 고려대학교 교우회 감사패 수상</h3>
+                                    <ul class="icon-list">
+                                        <li class="calendar">2018-05-01</li>
+                                        <li class="eye">1,234</li>
+                                    </ul>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="d-none d-md-block col-md-6 col-xl-4">
+                            <a href="#">
+                                <div class="item">
+                                    <figure>
+                                        <img src="/assets/images/campaign/img_none_review.jpg" class="img-fluid" alt="">
+                                    </figure>
+                                </div>
+                                <div class="details">
+                                    <small>해외아동결연</small>
+                                    <h3>사랑의의료봉사, 고려대학교 교우회 감사패 수상</h3>
+                                    <ul class="icon-list">
+                                        <li class="calendar">2018-05-01</li>
+                                        <li class="eye">1,234</li>
+                                    </ul>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="d-none d-md-block col-md-6 col-xl-4">
+                            <a href="#">
+                                <div class="item">
+                                    <figure>
+                                        <img src="/assets/images/campaign/img_review03.jpg" class="img-fluid" alt="">
+                                    </figure>
+                                </div>
+                                <div class="details">
+                                    <small>해외아동결연</small>
+                                    <h3>사랑의의료봉사, 고려대학교 교우회 감사패 수상</h3>
+                                    <ul class="icon-list">
+                                        <li class="calendar">2018-05-01</li>
+                                        <li class="eye">1,234</li>
+                                    </ul>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                    <nav class="paging">
+                        <ol class="pagination">
+                            <li class="first">
+                                <a href="#">
+                                    <i class="icon-angle-double-left">
+                                        <span class="sr-only">첫 페이지</span>
+                                    </i>
+                                </a>
+                            </li>
+                            <li class="prev">
+                                <a href="#">
+                                    <i class="icon-angle-left">
+                                        <span class="sr-only">이전 페이지</span>
+                                    </i>
+                                </a>
+                            </li>
+                            <li class="active">
+                                <a href="#">1</a>
+                            </li>
+                            <li>
+                                <a href="#">2</a>
+                            </li>
+                            <li>
+                                <a href="#">3</a>
+                            </li>
+                            <li>
+                                <a href="#">4</a>
+                            </li>
+                            <li>
+                                <a href="#">5</a>
+                            </li>
+                            <li class="next">
+                                <a href="#">
+                                    <i class="icon-angle-right">
+                                        <span class="sr-only">다음 페이지</span>
+                                    </i>
+                                </a>
+                            </li>
+                            <li class="last">
+                                <a href="#">
+                                    <i class="icon-angle-double-right">
+                                        <span class="sr-only">마지막 페이지</span>
+                                    </i>
+                                </a>
+                            </li>
+                        </ol>	
+                    </nav>
+                </div>
+            </article>
+    	</div>
+    </main>
+    
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/sitemap.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+    <script src="/assets/js/sub.js"></script>
+</body>
+</html>
