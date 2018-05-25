8b3bae8cc6bd73841344818bfeb96746b9302b5d
Joahra <mk_ahra0522@gmail.com>
2018-05-25 17:36:20 +0900

    아코디언 작업중...

---
+++ introduce/corporation.php
@@ -0,0 +1,46 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link rel="stylesheet" href="/assets/css/sub.css">
+<link rel="stylesheet" href="/assets/css/introduce.css">
+</head>
+<body class="sub introduce">
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/aside.php'); ?>
+    
+    <!-- page-header -->
+    <div class="page-header">
+        <div class="page-summary">
+            <h2>We bring <span>hope</span> to neighbors<br>in despair around the world</h2>
+        </div>
+        <div class="container">
+            <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/_lnb.php'); ?>
+        </div>
+    </div>
+    
+    <!-- page-intro -->
+    <div class="page-intro">
+        <div class="container">
+            <div class="page-title">
+                <h3>투명경영</h3>
+            </div>
+            <nav class="lnb">
+                <ul class="tab-list">
+                    <li class="active"><a href="/introduce/corporation.php">사단법인</a></li>
+                    <li><a href="#">복지법인</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    
+    <!-- Content -->
+	<main id="content" class="ambassadors">
+		<div class="container">
+			
+		</div>
+    </main>
+    
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/sitemap.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+    <script src="/assets/js/sub.js"></script>
+</body>
+</html>
