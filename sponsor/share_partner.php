6761055ebeeed0472ccaaa30813bfb5d6c30b322
Joahra <mk_ahra0522@gmail.com>
2018-05-28 18:35:31 +0900

    스폰서 작업중... (나눔가게 완료)

---
+++ sponsor/share_partner.php
@@ -0,0 +1,489 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link rel="stylesheet" href="/assets/css/sub.css">
+<link rel="stylesheet" href="/assets/css/sponsor.css">
+</head>
+<body class="sub sponsor">
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/aside.php'); ?>
+    
+    <!-- page-header -->
+    <div class="page-header">
+        <div class="page-summary">
+            <h2>We bring <span>hope</span> to neighbors<br>in despair around the world</h2>
+        </div>
+		<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/_lnb.php'); ?>
+    </div>
+    
+    <!-- page-intro -->
+    <div class="page-intro">
+        <div class="container">
+            <div class="page-title">
+                <h3>기업후원</h3>
+            </div>
+            <nav class="lnb">
+                <ul class="tab-list">
+                    <li class="active"><a href="/sponsor/share_partner.php">나눔파트너</a></li>
+                    <li><a href="/sponsor/share_store.php">나눔가게</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    
+    <!-- Content -->
+    <main id="content" class="share-partner">
+    	<div class="container">
+    		<div class="page-intro">
+    			<p>
+    				기업의 나눔은 선택이 아닌 의무가 되었으며, 현대사회에서 기업의 사회공헌활동은 점차 사회적으로 중요해지고 있습니다. <br class="d-md-none"><br>
+    				기업은 나눔을 통해 기업의 긍정적인 이미지를 구축, 사회에 널리 알리고 더 나아가 임직원들의 참여를 통해 나눔 문화를 함께 만들어가며,<br class="d-md-none"><br>
+    				나눔을 통한 기쁨을 함께 느낄 수 있습니다. 하나된 마음으로 나누는 세상을 만드는 굿피플과 Partnership을 통해 아름다운 지구촌 건설에 함께 동참할 나눔 파트너를 기다립니다.
+    			</p>
+    			<figure>
+    				<img src="/assets/images/sponser/img_partner_intro_sm.png" class="img-fluid d-md-none" alt="">
+    				<img src="/assets/images/sponser/img_partner_intro_md.png" class="img-fluid d-none d-md-block d-xl-none" alt="">
+    				<img src="/assets/images/sponser/img_partner_intro_xl.png" class="img-fluid d-none d-xl-block" alt=""> 
+    			</figure>
+    		</div>
+    		<ul class="notify">
+    			<li>
+    				<dl>
+    					<dt>사회공헌 캠페인</dt>
+    					<dd>
+    						굿피플은 기업과 함께 사회공헌캠페인(공익마케팅)을 펼칩니다.<br class="d-none d-md-block"> 파트너 기업의 특성에 맞는 사회공헌캠페인을 기획, 제공하며 함께 효과적인 운영을 통해 성공적인 캠페인 활동을 지원합니다.
+    					</dd>
+    				</dl>
+    			</li>
+    			<li>
+    				<dl>
+    					<dt>프로젝트 기금조성&#40;모금 / 물품&#41;</dt>
+    					<dd>
+    						굿피플은 진행중인 프로젝트별 기금조성을 기업과 함께합니다.<br class="d-none d-md-block"> 기업에 맞는 프로젝트를 선정, 기금을 조성하여 기업과 연계하여 지역개발에 기여하는 지원 방법입니다. 물품지원도 함께 이루어집니다.
+    					</dd>
+    				</dl>
+    			</li>
+    			<li>
+    				<dl>
+    					<dt>나눔파트너</dt>
+    					<dd>
+    						굿피플은 기업과 함께 나눔 캠페인, 임직원 자원봉사, 사업 후원, 국내‧해외 등 다양한 사회공헌활동을 진행하고 있습니다. 임직원들의 참여를 통한 나눔문화 확산, 기업의 긍정적인 이미지 구축 등 나눔을 통한 기쁨을 함께 느끼실 수 있습니다.
+    					</dd>
+    				</dl>
+    			</li>
+    			<li>
+    				<dl>
+    					<dt>나눔가게</dt>
+    					<dd>
+    						식당, 편의점, 약국, 은행 등 각 가게에 굿피플 모금함을 설치하여 어려운 이웃을 위한 나눔 활동에 동참하실 수 있습니다.
+    					</dd>
+    				</dl>
+    			</li>
+    		</ul>
+    		<div class="btn-area">
+    			<p>
+    				<a href="#" class="btn btn-default" role="button">나눔파트너, 나눔가게 문의하기</a>
+    			</p>
+    		</div>
+    		<ul class="ask">
+    			<li>
+    				<dl>
+    					<dt>나눔파트너 관련문의</dt>
+    					<dd>02-6011-9466</dd>
+    				</dl>
+    			</li>
+    			<li>
+    				<a href="mailto:help@goodpeople.or.kr">help@goodpeople.or.kr</a>
+    			</li> 
+    		</ul>
+    		<div class="tab-sponser">
+				<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+					<li class="nav-item">
+						<a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">분류 1</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">분류 2</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">분류 3</a>
+					</li>
+				</ul>
+				<div class="tab-content" id="pills-tabContent">
+					<div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+						<ul class="row">
+							<li class="col-12 col-md-4 col-xl-3">
+								<div class="sponsor-company">
+									<div class="item">
+										<img src="/assets/images/sponser/img_partner01.jpg" class="img-fluid" alt="">
+									</div>
+									<p>롯데면세점</p>
+								</div>
+							</li>
+							<li class="col-12 col-md-4 col-xl-3">
+								<div class="sponsor-company">
+									<div class="item">
+										<img src="/assets/images/sponser/img_partner02.jpg" class="img-fluid" alt="">
+									</div>
+									<p>호텔신라</p>
+								</div>
+							</li>
+							<li class="col-12 col-md-4 col-xl-3">
+								<div class="sponsor-company">
+									<div class="item">
+										<img src="/assets/images/sponser/img_partner03.jpg" class="img-fluid" alt="">
+									</div>
+									<p>참존</p>
+								</div>
+							</li>
+							<li class="col-12 col-md-4 col-xl-3">
+								<div class="sponsor-company">
+									<div class="item">
+										<img src="/assets/images/sponser/img_partner04.jpg" class="img-fluid" alt="">
+									</div>
+									<p>리젠</p>
+								</div>
+							</li>
+							<li class="col-12 col-md-4 col-xl-3">
+								<div class="sponsor-company">
+									<div class="item">
+										<img src="/assets/images/sponser/img_partner03.jpg" class="img-fluid" alt="">
+									</div>
+									<p>참존</p>
+								</div>
+							</li>
+							<li class="col-12 col-md-4 col-xl-3">
+								<div class="sponsor-company">
+									<div class="item">
+										<img src="/assets/images/sponser/img_partner05.jpg" class="img-fluid" alt="">
+									</div>
+									<p>SK 네트윅스</p>
+								</div>
+							</li>
+							<li class="col-12 col-md-4 col-xl-3">
+								<div class="sponsor-company">
+									<div class="item">
+										<img src="/assets/images/sponser/img_partner06.jpg" class="img-fluid" alt="">
+									</div>
+									<p>이랜드</p>
+								</div>
+							</li>
+							<li class="col-12 col-md-4 col-xl-3">
+								<div class="sponsor-company">
+									<div class="item">
+										<img src="/assets/images/sponser/img_partner07.jpg" class="img-fluid" alt="">
+									</div>
+									<p>엔타스 듀티프리</p>
+								</div>
+							</li>
+							<li class="col-12 col-md-4 col-xl-3">
+								<div class="sponsor-company">
+									<div class="item">
+										<img src="/assets/images/sponser/img_partner01.jpg" class="img-fluid" alt="">
+									</div>
+									<p>롯데면세점</p>
+								</div>
+							</li>
+							<li class="col-12 col-md-4 col-xl-3">
+								<div class="sponsor-company">
+									<div class="item">
+										<img src="/assets/images/sponser/img_partner02.jpg" class="img-fluid" alt="">
+									</div>
+									<p>호텔신라</p>
+								</div>
+							</li>
+							<li class="col-12 col-md-4 col-xl-3">
+								<div class="sponsor-company">
+									<div class="item">
+										<img src="/assets/images/sponser/img_partner03.jpg" class="img-fluid" alt="">
+									</div>
+									<p>참존</p>
+								</div>
+							</li>
+						</ul>
+						<nav class="paging">
+							<ol class="pagination">
+								<li class="first">
+									<a href="#">
+										<i class="icon-angle-double-left">
+											<span class="sr-only">첫 페이지</span>
+										</i>
+									</a>
+								</li>
+								<li class="prev">
+									<a href="#">
+										<i class="icon-angle-left">
+											<span class="sr-only">이전 페이지</span>
+										</i>
+									</a>
+								</li>
+								<li class="active">
+									<a href="#">1</a>
+								</li>
+								<li>
+									<a href="#">2</a>
+								</li>
+								<li>
+									<a href="#">3</a>
+								</li>
+								<li>
+									<a href="#">4</a>
+								</li>
+								<li>
+									<a href="#">5</a>
+								</li>
+								<li class="next">
+									<a href="#">
+										<i class="icon-angle-right">
+											<span class="sr-only">다음 페이지</span>
+										</i>
+									</a>
+								</li>
+								<li class="last">
+									<a href="#">
+										<i class="icon-angle-double-right">
+											<span class="sr-only">마지막 페이지</span>
+										</i>
+									</a>
+								</li>
+							</ol>	
+						</nav>
+					</div>
+					<div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+						<ul class="row">
+							<li class="col-12 col-md-4 col-xl-3">
+								<div class="sponsor-company">
+									<div class="item">
+										<img src="/assets/images/sponser/img_partner01.jpg" class="img-fluid" alt="">
+									</div>
+									<p>롯데면세점</p>
+								</div>
+							</li>
+							<li class="col-12 col-md-4 col-xl-3">
+								<div class="sponsor-company">
+									<div class="item">
+										<img src="/assets/images/sponser/img_partner02.jpg" class="img-fluid" alt="">
+									</div>
+									<p>호텔신라</p>
+								</div>
+							</li>
+							<li class="col-12 col-md-4 col-xl-3">
+								<div class="sponsor-company">
+									<div class="item">
+										<img src="/assets/images/sponser/img_partner03.jpg" class="img-fluid" alt="">
+									</div>
+									<p>참존</p>
+								</div>
+							</li>
+							<li class="col-12 col-md-4 col-xl-3">
+								<div class="sponsor-company">
+									<div class="item">
+										<img src="/assets/images/sponser/img_partner04.jpg" class="img-fluid" alt="">
+									</div>
+									<p>리젠</p>
+								</div>
+							</li>
+							<li class="col-12 col-md-4 col-xl-3">
+								<div class="sponsor-company">
+									<div class="item">
+										<img src="/assets/images/sponser/img_partner03.jpg" class="img-fluid" alt="">
+									</div>
+									<p>참존</p>
+								</div>
+							</li>
+							<li class="col-12 col-md-4 col-xl-3">
+								<div class="sponsor-company">
+									<div class="item">
+										<img src="/assets/images/sponser/img_partner05.jpg" class="img-fluid" alt="">
+									</div>
+									<p>SK 네트윅스</p>
+								</div>
+							</li>
+							<li class="col-12 col-md-4 col-xl-3">
+								<div class="sponsor-company">
+									<div class="item">
+										<img src="/assets/images/sponser/img_partner06.jpg" class="img-fluid" alt="">
+									</div>
+									<p>이랜드</p>
+								</div>
+							</li>
+							<li class="col-12 col-md-4 col-xl-3">
+								<div class="sponsor-company">
+									<div class="item">
+										<img src="/assets/images/sponser/img_partner07.jpg" class="img-fluid" alt="">
+									</div>
+									<p>엔타스 듀티프리</p>
+								</div>
+							</li>
+						</ul>
+						<nav class="paging">
+							<ol class="pagination">
+								<li class="first">
+									<a href="#">
+										<i class="icon-angle-double-left">
+											<span class="sr-only">첫 페이지</span>
+										</i>
+									</a>
+								</li>
+								<li class="prev">
+									<a href="#">
+										<i class="icon-angle-left">
+											<span class="sr-only">이전 페이지</span>
+										</i>
+									</a>
+								</li>
+								<li class="active">
+									<a href="#">1</a>
+								</li>
+								<li>
+									<a href="#">2</a>
+								</li>
+								<li>
+									<a href="#">3</a>
+								</li>
+								<li>
+									<a href="#">4</a>
+								</li>
+								<li>
+									<a href="#">5</a>
+								</li>
+								<li class="next">
+									<a href="#">
+										<i class="icon-angle-right">
+											<span class="sr-only">다음 페이지</span>
+										</i>
+									</a>
+								</li>
+								<li class="last">
+									<a href="#">
+										<i class="icon-angle-double-right">
+											<span class="sr-only">마지막 페이지</span>
+										</i>
+									</a>
+								</li>
+							</ol>	
+						</nav>
+					</div>
+					<div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
+						<ul class="row">
+							<li class="col-12 col-md-4 col-xl-3">
+								<div class="sponsor-company">
+									<div class="item">
+										<img src="/assets/images/sponser/img_partner01.jpg" class="img-fluid" alt="">
+									</div>
+									<p>롯데면세점</p>
+								</div>
+							</li>
+							<li class="col-12 col-md-4 col-xl-3">
+								<div class="sponsor-company">
+									<div class="item">
+										<img src="/assets/images/sponser/img_partner02.jpg" class="img-fluid" alt="">
+									</div>
+									<p>호텔신라</p>
+								</div>
+							</li>
+							<li class="col-12 col-md-4 col-xl-3">
+								<div class="sponsor-company">
+									<div class="item">
+										<img src="/assets/images/sponser/img_partner03.jpg" class="img-fluid" alt="">
+									</div>
+									<p>참존</p>
+								</div>
+							</li>
+							<li class="col-12 col-md-4 col-xl-3">
+								<div class="sponsor-company">
+									<div class="item">
+										<img src="/assets/images/sponser/img_partner04.jpg" class="img-fluid" alt="">
+									</div>
+									<p>리젠</p>
+								</div>
+							</li>
+							<li class="col-12 col-md-4 col-xl-3">
+								<div class="sponsor-company">
+									<div class="item">
+										<img src="/assets/images/sponser/img_partner03.jpg" class="img-fluid" alt="">
+									</div>
+									<p>참존</p>
+								</div>
+							</li>
+							<li class="col-12 col-md-4 col-xl-3">
+								<div class="sponsor-company">
+									<div class="item">
+										<img src="/assets/images/sponser/img_partner05.jpg" class="img-fluid" alt="">
+									</div>
+									<p>SK 네트윅스</p>
+								</div>
+							</li>
+							<li class="col-12 col-md-4 col-xl-3">
+								<div class="sponsor-company">
+									<div class="item">
+										<img src="/assets/images/sponser/img_partner06.jpg" class="img-fluid" alt="">
+									</div>
+									<p>이랜드</p>
+								</div>
+							</li>
+							<li class="col-12 col-md-4 col-xl-3">
+								<div class="sponsor-company">
+									<div class="item">
+										<img src="/assets/images/sponser/img_partner07.jpg" class="img-fluid" alt="">
+									</div>
+									<p>엔타스 듀티프리</p>
+								</div>
+							</li>
+						</ul>
+						<nav class="paging">
+							<ol class="pagination">
+								<li class="first">
+									<a href="#">
+										<i class="icon-angle-double-left">
+											<span class="sr-only">첫 페이지</span>
+										</i>
+									</a>
+								</li>
+								<li class="prev">
+									<a href="#">
+										<i class="icon-angle-left">
+											<span class="sr-only">이전 페이지</span>
+										</i>
+									</a>
+								</li>
+								<li class="active">
+									<a href="#">1</a>
+								</li>
+								<li>
+									<a href="#">2</a>
+								</li>
+								<li>
+									<a href="#">3</a>
+								</li>
+								<li>
+									<a href="#">4</a>
+								</li>
+								<li>
+									<a href="#">5</a>
+								</li>
+								<li class="next">
+									<a href="#">
+										<i class="icon-angle-right">
+											<span class="sr-only">다음 페이지</span>
+										</i>
+									</a>
+								</li>
+								<li class="last">
+									<a href="#">
+										<i class="icon-angle-double-right">
+											<span class="sr-only">마지막 페이지</span>
+										</i>
+									</a>
+								</li>
+							</ol>	
+						</nav>
+					</div>
+				</div>
+    		</div>
+    	</div>
+    </main>
+    
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/sitemap.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+    <script src="/assets/js/sub.js"></script>
+</body>
+</html>
---
+++ sponsor/share_store.php
@@ -0,0 +1,199 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link rel="stylesheet" href="/assets/css/sub.css">
+<link rel="stylesheet" href="/assets/css/sponsor.css">
+</head>
+<body class="sub sponsor">
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/aside.php'); ?>
+    
+    <!-- page-header -->
+    <div class="page-header">
+        <div class="page-summary">
+            <h2>We bring <span>hope</span> to neighbors<br>in despair around the world</h2>
+        </div>
+		<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/_lnb.php'); ?>
+    </div>
+    
+    <!-- page-intro -->
+    <div class="page-intro">
+        <div class="container">
+            <div class="page-title">
+                <h3>기업후원</h3>
+            </div>
+            <nav class="lnb">
+                <ul class="tab-list">
+                    <li><a href="/sponsor/partner_introduce.php">나눔파트너</a></li>
+                    <li class="active"><a href="/sponsor/share_store.php">나눔가게</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    
+    <!-- Content -->
+    <main id="content" class="share-store">
+    	<div class="container">
+			<div class="board-list-1">
+				<ul>
+					<li>
+						<a href="#">
+							<div class="board-info">
+								<div class="row">
+									<div class="col-12 col-md-6 col-xl-5">
+										<figure>
+											<img src="/assets/images/sub/img_board01.jpg" class="img-fluid" alt="">
+										</figure>
+									</div>
+									<div class="col-12 col-md-6 col-xl-7">
+										<div class="details">
+											<h4>사랑의의료봉사, 고려대학교 교우회 감사패 수상</h4>
+											<p>
+												굿피플 사랑의의료봉사가 지난 4월 21일 고려대학교 교우회로부터 감사패를 수상했습니다
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<div class="board-info">
+								<div class="row">
+									<div class="col-12 col-md-6 col-xl-5">
+										<figure>
+											<img src="/assets/images/sub/img_board_none.jpg" alt="">
+										</figure>
+									</div>
+									<div class="col-12 col-md-6 col-xl-7">
+										<div class="details">
+											<h4>한국코와주식회사, 굿피플과 함께 취약계층 건강증진에 나서</h4>
+											<p>
+												굿피플에 전해진 종합비타민 '큐피코와골드α정' 25,000개가 소외계층을 위해 사용되었습니다.
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<div class="board-info">
+								<div class="row">
+									<div class="col-12 col-md-6 col-xl-5">
+										<figure>
+											<img src="/assets/images/sub/img_board_none.jpg" alt="">
+										</figure>
+									</div>
+									<div class="col-12 col-md-6 col-xl-7">
+										<div class="details">
+											<h4>2018 기부 마라톤 대회 봉사자 모집 공고</h4>
+											<p>
+												2018 기부마라톤대회에 굿피플과 함게할 자원봉사자 여러분을 모집하고 있습니다. 많은 관심과 참여...
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</a>
+					</li>
+					<li class="d-none d-md-block">
+						<a href="#">
+							<div class="board-info">
+								<div class="row">
+									<div class="col-12 col-md-6 col-xl-5">
+										<figure>
+											<img src="/assets/images/sub/img_board02.jpg" alt="">
+										</figure>
+									</div>
+									<div class="col-12 col-md-6 col-xl-7">
+										<div class="details">
+											<h4>방경현 목사 굿피플 아너스클럽에 가입</h4>
+											<p>
+												굿피플의 고액기부 후원자모임인 '굿피플 아너스클럽'에 여의도순복음교회 새성북성전 방경현 목사가 회원으로 가입했습니다.
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</a>
+					</li>
+					<li class="d-none d-md-block">
+						<a href="#">
+							<div class="board-info">
+								<div class="row">
+									<div class="col-12 col-md-6 col-xl-5">
+										<figure>
+											<img src="/assets/images/sub/img_board03.jpg" alt="">
+										</figure>
+									</div>
+									<div class="col-12 col-md-6 col-xl-7">
+										<div class="details">
+											<h4>아프리카 태양보다 뜨거웠던 나눔대사 위촉현장</h4>
+											<p>
+												굿피플이 배우 정준을 굿피플 나눔대사로 위촉했습니다.
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</a>
+					</li>
+				</ul>
+			</div>
+			<nav class="paging">
+				<ol class="pagination">
+					<li class="first">
+						<a href="#">
+							<i class="icon-angle-double-left">
+								<span class="sr-only">첫 페이지</span>
+							</i>
+						</a>
+					</li>
+					<li class="prev">
+						<a href="#">
+							<i class="icon-angle-left">
+								<span class="sr-only">이전 페이지</span>
+							</i>
+						</a>
+					</li>
+					<li class="active">
+						<a href="#">1</a>
+					</li>
+					<li>
+						<a href="#">2</a>
+					</li>
+					<li>
+						<a href="#">3</a>
+					</li>
+					<li>
+						<a href="#">4</a>
+					</li>
+					<li>
+						<a href="#">5</a>
+					</li>
+					<li class="next">
+						<a href="#">
+							<i class="icon-angle-right">
+								<span class="sr-only">다음 페이지</span>
+							</i>
+						</a>
+					</li>
+					<li class="last">
+						<a href="#">
+							<i class="icon-angle-double-right">
+								<span class="sr-only">마지막 페이지</span>
+							</i>
+						</a>
+					</li>
+				</ol>	
+			</nav>
+    	</div>
+    </main>
+    
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/sitemap.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+    <script src="/assets/js/sub.js"></script>
+</body>
+</html>
