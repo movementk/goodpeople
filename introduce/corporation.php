9c23a9422d6ef2a961bd40df43dac21b9337fc15
Joahra <mk_ahra0522@gmail.com>
2018-05-29 18:26:31 +0900

    찾아오시는길 작업중...

--- introduce/corporation.php
+++ introduce/corporation.php
@@ -22,17 +22,419 @@
             </div>
             <nav class="lnb">
                 <ul class="tab-list">
-                    <li class="active"><a href="/introduce/corporation.php">사단법인</a></li>
-                    <li><a href="#">복지법인</a></li>
+                    <li class="active"><a href="/introduce/corporation.php">나눔대사</a></li>
+                    <li><a href="/introduce/welfarecorp.php">복지법인</a></li>
                 </ul>
             </nav>
         </div>
     </div>
     
     <!-- Content -->
-	<main id="content" class="ambassadors">
+	<main id="content" class="corporation">
 		<div class="container">
-			
+			<section>
+				<div class="section-header">
+					<strong>소중한 후원금, 투명하게 효율적으로 운용합니다.</strong>
+				</div>
+				<div class="section-content">
+					<article class="accordion" id="accordion">
+						<div class="card">
+							<div class="card-header" id="headingOne">
+								<a href="#" class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+									<strong>2017년 사업보고서</strong>
+									<p>
+										2017년도 굿피플의 연간 수입총계는
+										<span>21,153,000,000</span>원이며, 
+										다음과 같이 사용되었습니다.
+									</p>
+								</a>
+							</div>
+							<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+								<div class="card-body">
+									<div class="btn-area">
+										<p>
+											<a href="#" class="btn btn-blue" role="button">보고서 PDF</a>
+											<a href="#" class="btn btn-gray" role="button">뷰어 다운로드</a>
+										</p>
+									</div>
+									<div class="chart-area">
+										<div class="row">
+											<div class="col-12 col-xl-6">
+												<img src="/assets/images/sub/img_chart01.png" class="img-fluid" alt="">
+											</div>
+											<div class="col-12 col-xl-6">
+												<img src="/assets/images/sub/img_chart02.png" class="img-fluid" alt="">
+											</div>
+											<div class="col-12 col-xl-6">
+												<img src="/assets/images/sub/img_chart01.png" class="img-fluid" alt="">
+											</div>
+											<div class="col-12 col-xl-6">
+												<img src="/assets/images/sub/img_chart02.png" class="img-fluid" alt="">
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header" id="headingTwo">
+								<a href="#" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+									<strong>2016년 사업보고서</strong>
+									<p>
+										2016년도 굿피플의 연간 수입총계는
+										<span>21,153,000,000</span>원이며, 
+										다음과 같이 사용되었습니다.
+									</p>
+								</a>
+							</div>
+							<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+								<div class="card-body">
+									<div class="btn-area">
+										<p>
+											<a href="#" class="btn btn-blue" role="button">보고서 PDF</a>
+											<a href="#" class="btn btn-gray" role="button">뷰어 다운로드</a>
+										</p>
+									</div>
+									<div class="chart-area">
+										<div class="row">
+											<div class="col-12 col-xl-6">
+												<img src="/assets/images/sub/img_chart01.png" class="img-fluid" alt="">
+											</div>
+											<div class="col-12 col-xl-6">
+												<img src="/assets/images/sub/img_chart02.png" class="img-fluid" alt="">
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header" id="headingThree">
+								<a href="#" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+									<strong>2015년 사업보고서</strong>
+									<p>
+										2015년도 굿피플의 연간 수입총계는
+										<span>21,153,000,000</span>원이며, 
+										다음과 같이 사용되었습니다.
+
+										2015년도 굿피플의 연간 수입총계는
+										<span>21,153,000,000</span>원이며, 
+										다음과 같이 사용되었습니다.
+									</p>
+								</a>
+							</div>
+							<div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+								<div class="card-body">
+									<div class="btn-area">
+										<p>
+											<a href="#" class="btn btn-blue" role="button">보고서 PDF</a>
+											<a href="#" class="btn btn-gray" role="button">뷰어 다운로드</a>
+										</p>
+									</div>
+									<div class="chart-area">
+										<div class="row">
+											<div class="col-12 col-xl-6">
+												<img src="/assets/images/sub/img_chart01.png" class="img-fluid" alt="">
+											</div>
+											<div class="col-12 col-xl-6">
+												<img src="/assets/images/sub/img_chart02.png" class="img-fluid" alt="">
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header" id="headingFour">
+								<a href="#" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFour" aria-expanded="true" aria-controls="collapseTwo">
+									<strong>2014년 사업보고서</strong>
+									<p>
+										2014년도 굿피플의 연간 수입총계는
+										<span>21,153,000,000</span>원이며, 
+										다음과 같이 사용되었습니다.
+									</p>
+								</a>
+							</div>
+							<div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
+								<div class="card-body">
+									<div class="btn-area">
+										<p>
+											<a href="#" class="btn btn-blue" role="button">보고서 PDF</a>
+											<a href="#" class="btn btn-gray" role="button">뷰어 다운로드</a>
+										</p>
+									</div>
+									<div class="chart-area">
+										<div class="row">
+											<div class="col-12 col-xl-6">
+												<img src="/assets/images/sub/img_chart01.png" class="img-fluid" alt="">
+											</div>
+											<div class="col-12 col-xl-6">
+												<img src="/assets/images/sub/img_chart02.png" class="img-fluid" alt="">
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header" id="headingFive">
+								<a href="#" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFive" aria-expanded="true" aria-controls="collapseTwo">
+									<strong>2013년 사업보고서</strong>
+									<p>
+										2013년도 굿피플의 연간 수입총계는
+										<span>21,153,000,000</span>원이며, 
+										다음과 같이 사용되었습니다.
+									</p>
+								</a>
+							</div>
+							<div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordion">
+								<div class="card-body">
+									<div class="btn-area">
+										<p>
+											<a href="#" class="btn btn-blue" role="button">보고서 PDF</a>
+											<a href="#" class="btn btn-gray" role="button">뷰어 다운로드</a>
+										</p>
+									</div>
+									<div class="chart-area">
+										<div class="row">
+											<div class="col-12 col-xl-6">
+												<img src="/assets/images/sub/img_chart01.png" class="img-fluid" alt="">
+											</div>
+											<div class="col-12 col-xl-6">
+												<img src="/assets/images/sub/img_chart02.png" class="img-fluid" alt="">
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header" id="headingSix">
+								<a href="#" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseSix" aria-expanded="true" aria-controls="collapseTwo">
+									<strong>2012년 사업보고서</strong>
+									<p>
+										2012년도 굿피플의 연간 수입총계는
+										<span>21,153,000,000</span>원이며, 
+										다음과 같이 사용되었습니다.
+									</p>
+								</a>
+							</div>
+							<div id="collapseSix" class="collapse" aria-labelledby="headingSix" data-parent="#accordion">
+								<div class="card-body">
+									<div class="btn-area">
+										<p>
+											<a href="#" class="btn btn-blue" role="button">보고서 PDF</a>
+											<a href="#" class="btn btn-gray" role="button">뷰어 다운로드</a>
+										</p>
+									</div>
+									<div class="chart-area">
+										<div class="row">
+											<div class="col-12 col-xl-6">
+												<img src="/assets/images/sub/img_chart01.png" class="img-fluid" alt="">
+											</div>
+											<div class="col-12 col-xl-6">
+												<img src="/assets/images/sub/img_chart02.png" class="img-fluid" alt="">
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header" id="headingSeven">
+								<a href="#" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseSeven" aria-expanded="true" aria-controls="collapseTwo">
+									<strong>2011년 사업보고서</strong>
+									<p>
+										2011년도 굿피플의 연간 수입총계는
+										<span>21,153,000,000</span>원이며, 
+										다음과 같이 사용되었습니다.
+									</p>
+								</a>
+							</div>
+							<div id="collapseSeven" class="collapse" aria-labelledby="headingSeven" data-parent="#accordion">
+								<div class="card-body">
+									<div class="btn-area">
+										<p>
+											<a href="#" class="btn btn-blue" role="button">보고서 PDF</a>
+											<a href="#" class="btn btn-gray" role="button">뷰어 다운로드</a>
+										</p>
+									</div>
+									<div class="chart-area">
+										<div class="row">
+											<div class="col-12 col-xl-6">
+												<img src="/assets/images/sub/img_chart01.png" class="img-fluid" alt="">
+											</div>
+											<div class="col-12 col-xl-6">
+												<img src="/assets/images/sub/img_chart02.png" class="img-fluid" alt="">
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header" id="headingEight">
+								<a href="#" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseEight" aria-expanded="true" aria-controls="collapseTwo">
+									<strong>2010년 사업보고서</strong>
+									<p>
+										2010년도 굿피플의 연간 수입총계는
+										<span>21,153,000,000</span>원이며, 
+										다음과 같이 사용되었습니다.
+									</p>
+								</a>
+							</div>
+							<div id="collapseEight" class="collapse" aria-labelledby="headingEight" data-parent="#accordion">
+								<div class="card-body">
+									<div class="btn-area">
+										<p>
+											<a href="#" class="btn btn-blue" role="button">보고서 PDF</a>
+											<a href="#" class="btn btn-gray" role="button">뷰어 다운로드</a>
+										</p>
+									</div>
+									<div class="chart-area">
+										<div class="row">
+											<div class="col-12 col-xl-6">
+												<img src="/assets/images/sub/img_chart01.png" class="img-fluid" alt="">
+											</div>
+											<div class="col-12 col-xl-6">
+												<img src="/assets/images/sub/img_chart02.png" class="img-fluid" alt="">
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header" id="headingNine">
+								<a href="#" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseNine" aria-expanded="true" aria-controls="collapseTwo">
+									<strong>2017년 사업보고서</strong>
+									<p>
+										2017년도 굿피플의 연간 수입총계는
+										<span>21,153,000,000</span>원이며, 
+										다음과 같이 사용되었습니다.
+									</p>
+								</a>
+							</div>
+							<div id="collapseNine" class="collapse" aria-labelledby="headingNine" data-parent="#accordion">
+								<div class="card-body">
+									<div class="btn-area">
+										<p>
+											<a href="#" class="btn btn-blue" role="button">보고서 PDF</a>
+											<a href="#" class="btn btn-gray" role="button">뷰어 다운로드</a>
+										</p>
+									</div>
+									<div class="chart-area">
+										<div class="row">
+											<div class="col-12 col-xl-6">
+												<img src="/assets/images/sub/img_chart01.png" class="img-fluid" alt="">
+											</div>
+											<div class="col-12 col-xl-6">
+												<img src="/assets/images/sub/img_chart02.png" class="img-fluid" alt="">
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header" id="headingTen">
+								<a href="#" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTen" aria-expanded="true" aria-controls="collapseTwo">
+									<strong>2016년 사업보고서</strong>
+									<p>
+										2016년도 굿피플의 연간 수입총계는
+										<span>21,153,000,000</span>원이며, 
+										다음과 같이 사용되었습니다.
+									</p>
+								</a>
+							</div>
+							<div id="collapseTen" class="collapse" aria-labelledby="headingTen" data-parent="#accordion">
+								<div class="card-body">
+									<div class="btn-area">
+										<p>
+											<a href="#" class="btn btn-blue" role="button">보고서 PDF</a>
+											<a href="#" class="btn btn-gray" role="button">뷰어 다운로드</a>
+										</p>
+									</div>
+									<div class="chart-area">
+										<div class="row">
+											<div class="col-12 col-xl-6">
+												<img src="/assets/images/sub/img_chart01.png" class="img-fluid" alt="">
+											</div>
+											<div class="col-12 col-xl-6">
+												<img src="/assets/images/sub/img_chart02.png" class="img-fluid" alt="">
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header" id="headingEleven">
+								<a href="#" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseEleven" aria-expanded="true" aria-controls="collapseTwo">
+									<strong>2015년 사업보고서</strong>
+									<p>
+										2015년도 굿피플의 연간 수입총계는
+										<span>21,153,000,000</span>원이며, 
+										다음과 같이 사용되었습니다.
+									</p>
+								</a>
+							</div>
+							<div id="collapseEleven" class="collapse" aria-labelledby="headingEleven" data-parent="#accordion">
+								<div class="card-body">
+									<div class="btn-area">
+										<p>
+											<a href="#" class="btn btn-blue" role="button">보고서 PDF</a>
+											<a href="#" class="btn btn-gray" role="button">뷰어 다운로드</a>
+										</p>
+									</div>
+									<div class="chart-area">
+										<div class="row">
+											<div class="col-12 col-xl-6">
+												<img src="/assets/images/sub/img_chart01.png" class="img-fluid" alt="">
+											</div>
+											<div class="col-12 col-xl-6">
+												<img src="/assets/images/sub/img_chart02.png" class="img-fluid" alt="">
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header" id="headingTwelve">
+								<a href="#" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwelve" aria-expanded="true" aria-controls="collapseTwo">
+									<strong>2014년 사업보고서</strong>
+									<p>
+										2014년도 굿피플의 연간 수입총계는
+										<span>21,153,000,000</span>원이며, 
+										다음과 같이 사용되었습니다.
+									</p>
+								</a>
+							</div>
+							<div id="collapseTwelve" class="collapse" aria-labelledby="headingTwelve" data-parent="#accordion">
+								<div class="card-body">
+									<div class="btn-area">
+										<p>
+											<a href="#" class="btn btn-blue" role="button">보고서 PDF</a>
+											<a href="#" class="btn btn-gray" role="button">뷰어 다운로드</a>
+										</p>
+									</div>
+									<div class="chart-area">
+										<div class="row">
+											<div class="col-12 col-xl-6">
+												<img src="/assets/images/sub/img_chart01.png" class="img-fluid" alt="">
+											</div>
+											<div class="col-12 col-xl-6"> 
+												<img src="/assets/images/sub/img_chart02.png" class="img-fluid" alt="">
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</article>
+				</div>
+			</section>
 		</div>
     </main>
     
