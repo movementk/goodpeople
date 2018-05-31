971de34025651c28c6de87c0ba1ecd205bd125b6
Joahra <mk_ahra0522@gmail.com>
2018-05-31 16:14:23 +0900

    일시후원작업중...

---
+++ sponsor/temporary_sponsor.php
@@ -0,0 +1,54 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link rel="stylesheet" href="/assets/css/sub.css">
+<link rel="stylesheet" href="/assets/css/sponsor.css">
+</head>
+<body class="sub sponsor">
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/aside.php'); ?>
+    
+    <!-- page-header -->
+    <div class="page-header">
+        <div class="page-summary">
+            <h2>We bring <span>hope</span> to neighbors<br>in despair around the world</h2>
+        </div>
+        <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/_lnb.php'); ?>
+    </div>
+    
+    <!-- page-intro -->
+    <div class="page-intro">
+        <div class="container">
+            <div class="page-title">
+                <h3>일시후원</h3>
+                <p class="summary">
+                	굿피플과 시작하는 작은나눔, 후원자분들의 마음이 모여 전세계 곳곳에 희망의 씨앗이 됩니다.<br class="d-none d-xl-block">
+					우리 이웃들에게 삶의 희망을 심어주세요.
+                </p>
+            </div>
+        </div>
+    </div>
+    
+    <!-- Content -->
+	<main id="content" class="temporary-sponsor">
+		<div class="container">
+			<div class="method">
+				<ul class="row">
+					<li class="col-12 col-xl-6">
+						<div class="homepage">
+							<figure>
+								<img src="/assets/images/sponser/img_temporary_com.png" alt="">
+							</figure>
+							<h5>dfdf</h5>	
+							<p>dfjsdkfl</p>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+    </main>
+    
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/sitemap.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+    <script src="/assets/js/sub.js"></script>
+</body>
+</html>
