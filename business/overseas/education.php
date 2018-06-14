5d78e03fc6fb238aad0cbc93cacafc93ee75c7be
Joahra <mk_ahra0522@gmail.com>
2018-06-14 18:29:17 +0900

    사업소개 교육지원사업페이지 작업중...

--- business/overseas/education.php
+++ business/overseas/education.php
@@ -23,8 +23,8 @@
             <nav class="lnb tab-business">
                 <ul class="tab-list">
                     <li><a href="/business/overseas/children.php">아동후원사업</a></li>
-                    <li class="active"><a href="/business/overseas/medical.php">보건의료사업</a></li>
-                    <li><a href="/business/overseas/education.php">교육지원사업</a></li>
+                    <li><a href="/business/overseas/medical.php">보건의료사업</a></li>
+                    <li class="active"><a href="/business/overseas/education.php">교육지원사업</a></li>
                     <li><a href="#">지역개발사업</a></li>
                 </ul>
             </nav>
@@ -32,11 +32,10 @@
     </div>
     
     <!-- Content -->
-    <main id="content" class="medical">
+    <main id="content" class="education">
 		<section>
 			<div class="section-header sub-header">
 				<div class="container">
-					<h3>의료환경 개선사업</h3>
 					<p>
 						질병에 쉽게 노출되어 있는 제 3국가에 병원 및 보건소 건축, 의료시설 개선, 의료기기 제공, 의약품 및 의료진 제공을 통해 수혜지역의 의료시설을 개선하고 의료서비스를 제공받지 못하는 지역주민들에게 양질의 의료서비스를 제공하여 질병의 예방 및 죽음에 이르는 것을 막기 위해 의료시설 및 진료, 수술 등을 지원합니다.
 					</p>
@@ -48,12 +47,12 @@
 						<div class="row">
 							<div class="col-12 col-xl-6">
 								<figure>   
-									<img src="/assets/images/business/img_medical_item01.jpg" class="img-fluid" alt="">
+									<img src="/assets/images/business/img_education_item01.jpg" class="img-fluid" alt="">
 								</figure>
 							</div>
 							<div class="col-12 col-xl-6">
 								<figure>
-									<img src="/assets/images/business/img_medical_item02.jpg" class="img-fluid" alt="">
+									<img src="/assets/images/business/img_education_item02.jpg" class="img-fluid" alt="">
 								</figure>
 							</div>
 						</div>
@@ -93,67 +92,6 @@
 						</div>
 					</article>
 				</div>
-				<section class="blind">
-					<div class="container">
-						<div class="section-header">
-							<h3>세계실명예방단 사업</h3>
-							<p>
-								질병에 쉽게 노출되어 있는 제 3국가에 병원 및 보건소 건축, 의료시설 개선, 의료기기 제공, 의약품 및 의료진 제공을 통해 수혜지역의 의료시설을 개선하고<br class="d-none d-xl-block"> 의료서비스를 제공받지 못하는 지역주민들에게 양질의 의료서비스를 제공하여 질병의예방 및 죽음에 이르는 것을 막기 위해 의료시설 및 진료, 수술 등을 지원합니다.
-							</p>
-						</div>
-						<div class="section-content">
-							<ul class="row justify-content-center">
-								<li class="col-12 col-md-4">
-									<div class="figure">
-										<img src="/assets/images/business/img_blind_list01.png" alt="">
-									</div>
-									<div class="figcaption">
-										<h5>실명 예방단 프로젝트</h5>
-										<p>
-											중국, 베트남, 스리랑카, 몽골, 필리핀,<br> 
-											방글라데시 등 제3세계 저개발국가
-										</p>
-									</div>
-								</li>
-								<li class="col-12 col-md-4">
-									<div class="figure">
-										<img src="/assets/images/business/img_blind_list02.png" alt="">
-									</div>
-									<div class="figcaption">
-										<h5>총 10개국 26회, 1,682명 수술</h5>
-										<p>
-											2014년 기준 세계실명예방단 수혜자
-										</p>
-									</div>
-								</li>
-							</ul>
-							
-							<article class="dot-board">
-								<div class="article-header">
-									<h3>최근 활동 소식</h3>
-								</div>
-								<div class="article-content">
-									<ul>
-										<li>
-											<a href="#">
-												&lt;보고서&gt; 2016 상반기 해외지역개발사업 보고서 
-												&lt;보고서&gt; 2016 상반기 해외지역개발사업 보고서
-												&lt;보고서&gt; 2016 상반기 해외지역개발사업 보고서
-												&lt;보고서&gt; 2016 상반기 해외지역개발사업 보고서
-												&lt;보고서&gt; 2016 상반기 해외지역개발사업 보고서
-											</a>
-										</li>
-										<li>
-											<a href="#">
-												&lt;보고서&gt; 2016 상반기 해외지역개발사업 보고서
-											</a>
-										</li>
-									</ul>
-								</div>
-							</article>
-						</div>
-					</div>
-				</section>
 			</div>
 		</section>
     </main>
