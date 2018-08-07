96d0ad87535c569c6e1fc4be470ce0651ed3f7fb
kimyh5392 <mk.kimyh@gmail.com>
2018-08-07 12:05:33 +0900

    sub2.php 시안 작업 완료.

---
+++ sub2.php
@@ -0,0 +1,21 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link rel="stylesheet" href="/assets/css/sub.css">
+</head>
+<body class="sub">
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/aside.php'); ?>
+    <!-- Content -->
+    <main id="content" class="campaign">
+        <div class="container">
+            <figure>
+                <img src="/assets/images/sub/img_sub3.jpg" class="img-fluid" alt="">
+            </figure>
+        </div>
+    </main>
+    
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/sitemap.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+    <script src="/assets/js/sub.js"></script>
+</body>
+</html>
