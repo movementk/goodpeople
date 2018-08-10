8c7ea068ec9849bc9d8e60f4db0a93e61dd3d124
kimyh5392 <mk.kimyh@gmail.com>
2018-08-10 15:44:50 +0900

    sub2 완료

--- sub2.php
+++ sub2.php
@@ -1,20 +1,16 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
 <link rel="stylesheet" href="/assets/css/sub.css">
 </head>
-<body class="sub">
-    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
-    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/aside.php'); ?>
+<body class="sub sub-2">
     <!-- Content -->
     <main id="content" class="campaign">
+        <h1 class="page-logo"><a href="/"><img src="/assets/images/sub/logo.png" alt=""></a></h1>
         <div class="container">
             <figure>
                 <img src="/assets/images/sub/img_sub3.jpg" class="img-fluid" alt="">
             </figure>
         </div>
     </main>
-    
-    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/sitemap.php'); ?>
-    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
     <script src="/assets/js/sub.js"></script>
 </body>
